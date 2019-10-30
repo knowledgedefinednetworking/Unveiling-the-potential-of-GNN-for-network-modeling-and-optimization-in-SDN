9e81cf677d717acf83ed64d2d57cb4ad19e36409
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2019-10-30 13:26:39 -0600

    Got the demo to run.  Fixes #1

--- code/run_routenet.sh
+++ code/run_routenet.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+PATH_TO_DATA="../datasets/$2"
+
 if [[ "$1" = "tfrecords" ]]; then
 
-    python3 routenet_with_link_cap.py data -d /home/datasets/SIGCOMM/$2/
+    python3 routenet_with_link_cap.py data -d $PATH_TO_DATA
 
 fi
 
 if [[ "$1" = "train" ]]; then
 
-    python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=32,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /home/datasets/SIGCOMM/$2/tfrecords/train/*.tfrecords --train_steps $3 --eval_ /home/datasets/SIGCOMM/$2/tfrecords/evaluate/*.tfrecords --model_dir ./CheckPoints/$2
+    python3 routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=32,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  $PATH_TO_DATA/tfrecords/train/*.tfrecords --train_steps $3 --eval_ $PATH_TO_DATA/tfrecords/evaluate/*.tfrecords --model_dir ./CheckPoints/$2
 
 fi
 
